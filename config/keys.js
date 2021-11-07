60d9b8bf0072aa912f9d35ba3c0c6939d3c76382
Andrew Husar <andrewgusarnew@gmail.com>
2021-11-07 09:43:38 +0200

    init1

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,10 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./keys.prod');
-} else {
-    module.exports = require('./keys.dev');
-}
+// if (process.env.NODE_ENV === 'production') {
+//     module.exports = require('./keys.prod');
+// } else {
+//     module.exports = require('./keys.dev');
+// }
+
+module.exports = {
+    mongoURI: 'mongodb+srv://anderboom:anderboom_15091969@cluster0.339ej.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    jwt: 'dev-jwt',
+};
