5a089bb9786b979bd92e323635f0e35512cb2456
Andrew Husar <andrewgusarnew@gmail.com>
2021-12-27 16:07:54 +0200

    dash

--- client/src/app/dashboard/components/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  links = [];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
